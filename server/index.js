a0db9073510b2a6fec3daa034ece614fa40dda66
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-14 23:42:12 +0800

    update dependencies by added consolidate template engine & mongoose and modified indexjs

--- server/index.js
+++ server/index.js
@@ -1,21 +1,37 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+/*
+ * Koneksi Nodejs dengan MongoDB menggunakan Mongoose
+ *
+ * Author By IvanDjoh
+ * https://ivandjoh.com/
+ *
+ * License :  Free!!!
+ */
 
-// initialization
+
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
+      mongoose = require('mongoose'),
+      path = require('path');
+
+// Server Initialization
 const app = express();
 
-// Middleware for depedencies
-app.use(bodyParser.json());
-app.use(cors());
+app.configure(() => {
+    app.use(express.logger());
 
-// Routes
-const posts = require('./routes/api/posts');
-app.use('/api/posts', posts);
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT','DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-// create port
-const port = process.env.PORT || 5000;
-
-app.listen(port, function(){
-    console.log(`Server Running on Port : ${port}`)
-});
+        if('OPTION' == req.method){
+            res.send(200);
+        }
+        else {
+            next();
+        }
+    })
+    app.use(express.bodyParser());
+    app.use(express.methodOve)
+})
